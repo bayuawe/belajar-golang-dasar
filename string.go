01fe81bd55db0470b3d11fec34917ebd0eb763b9
bayuawe <bayuaryandi21@gmail.com>
2025-10-21 11:13:07 +0700

    learn: helloword, sample, number, boolean, string

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Bayu")
+	fmt.Println("Bayu Aryandi")
+	fmt.Println("Bayu Aryandi Wijaya")
+
+	fmt.Println(len("Bayu"))
+	fmt.Println("Bayu Aryandi"[0])
+	fmt.Println("Bayu Aryandi Wijaya"[2])
+}
